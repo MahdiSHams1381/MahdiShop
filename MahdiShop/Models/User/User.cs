f0359fee3d2204e55a93c4b62fcee9af3d9f2006
M-Shams <mahdishams.13811381@gmail.com>
2023-03-16 00:58:17 +0330

    creat user module

---
+++ MahdiShop/Models/User/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MahdiShop.Models.User
+{
+    public class User
+    {
+        [Required]
+        [Key]
+        public int UserId { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string UserName { get; set; }
+        [MaxLength(15)]
+        [DataType(DataType.PhoneNumber)]
+        public string UserPhone { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string UserEmail { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
