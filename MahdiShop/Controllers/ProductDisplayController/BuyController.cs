fd6e4d9bfbd58d3244074234ac7afec0162f8386
M-Shams <mahdishams.13811381@gmail.com>
2023-03-19 20:51:06 +0330

    creat pay (buy) cart

---
+++ MahdiShop/Controllers/ProductDisplayController/BuyController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace MahdiShop.Controllers.ProductDisplayController
+{
+    public class BuyController : Controller
+    {
+        public IActionResult Index(int id)
+        {
+            var c = new CookieOptions();
+            c.Expires = DateTime.Now.AddSeconds(100);
+            Response.Cookies.Append("product"+id, Convert.ToString(id), c);
+            return Redirect("../../ProductDisplay");
+        }
+    }
+}
