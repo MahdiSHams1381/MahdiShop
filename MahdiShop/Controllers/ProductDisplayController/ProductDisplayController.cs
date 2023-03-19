2ade7ea8710a02fdc95da382e3796165a39812b9
M-Shams <mahdishams.13811381@gmail.com>
2023-03-19 12:48:23 +0330

    add product (controller and view)

---
+++ MahdiShop/Controllers/ProductDisplayController/ProductDisplayController.cs
@@ -0,0 +1,25 @@
+﻿using MahdiShop.Data;
+using MahdiShop.Models.Product;
+using MahdiShop.Views.ViewModule;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MahdiShop.Controllers.ProductLauncher
+{
+    public class ProductDisplayController : Controller
+    {
+        private Context _context;
+        public ProductDisplayController(Context context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Product> p = _context.Product.Select(c => c).ToList();
+            ProductFilterViewModel k = new ProductFilterViewModel();
+            k.productss = p;
+            return View(k);
+        }
+    }
+}
