7179cc51404a4ebf34261b5e6c0326847193136a
M-Shams <mahdishams.13811381@gmail.com>
2023-05-18 12:30:50 +0430

    creat categoryRipo

---
+++ Infrastracture/Interface/ICategoryRipo.cs
@@ -0,0 +1,19 @@
+﻿using MahdiShop.DataLayer.Models.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastracture.Interface
+{
+    public interface ICategoryRipo
+    {
+        bool AddCategory(Category category);
+
+        bool DeleteCategory(int id);
+
+        bool UpdeteCategory(Category category);
+        Category SearchCategoryById(int id);
+    }
+}
---
+++ Services/Repository/CategoryRipo.cs
@@ -0,0 +1,56 @@
+﻿using Infrastracture.Interface;
+using MahdiShop.DataLayer.Data;
+using MahdiShop.DataLayer.Models.Product;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Repository
+{
+    internal class CategoryRipo : ICategoryRipo
+    {
+        private Context _context;
+        public CategoryRipo(Context context) { _context = context; }
+        public bool AddCategory(Category category)
+        {
+            try {
+                _context.Category.Add(category);
+                _context.SaveChanges();
+                return true;
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteCategory(int id)
+        {
+            try
+            {
+                _context.Category.Add(SearchCategoryById(id));
+                _context.SaveChanges();
+                return true;
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public Category SearchCategoryById(int id)
+        {
+            return _context.Category.Find(id);
+        }
+
+        public bool UpdeteCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
